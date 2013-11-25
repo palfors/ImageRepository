73cfb89b821c193c236692010f58d68b364d68e4
Pete Alfors <peter.alfors@kohls.com>
2013-11-24 19:42:16 -0600

    introduction of basic application exception

---
+++ src/main/java/com/alfors/imagerepository/ImageRepositoryException.java
@@ -0,0 +1,12 @@
+package com.alfors.imagerepository;
+
+/**
+ * Parent application exception
+ */
+public class ImageRepositoryException extends Exception {
+
+    public ImageRepositoryException(String message)
+    {
+        super(message);
+    }
+}
